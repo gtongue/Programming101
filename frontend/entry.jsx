df8e2160ab49aa2bb96e5f167cb5c65c78534c30
Garrett Tongue <garrettjtongue@gmail.com>
2017-11-18 16:51:17 -0800

    Add API calls for user and session

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {deleteSession, postSession, postUser} from './utils/api/user_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-
+  window.postSession = postSession;
+  window.deleteSession = deleteSession;
+  window.postUser = postUser;
   ReactDOM.render(<div> React is god</div>, root);
 });
