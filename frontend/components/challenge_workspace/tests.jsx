a17f5c99a6c37222c609130e4153aa8a79c0d528
Garrett Tongue <garrettjtongue@gmail.com>
2017-11-23 10:45:04 -0800

    Add demo user in seed file and Setup initial test-container

---
+++ frontend/components/challenge_workspace/tests.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Tests extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className = "tests-container">
+        Hello there I am the testing section!
+      </div>
+    );
+  }
+}
+
+export default Tests;
---
+++ frontend/components/challenge_workspace/tests_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import Tests from './tests';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tests);
